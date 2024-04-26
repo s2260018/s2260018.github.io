536312bbea3125f0c3147945efc3864d50546391
Jong <JongL@Physics.LeidenUniv.nl>
2024-04-26 12:40:17 +0200

    improve style
    - Add css based on simple.css;
    - Use fancy html5;
    - Remove validation link;
    - remove creation date from planning;
    - Add downscaled version of breadboard image.

---
+++ .dir-locals.el
@@ -0,0 +1,4 @@
+((org-mode . ((org-html-head . "<link rel=\"stylesheet\" href=\"/simple.css\">")
+	      (org-html-doctype . "html5")
+	      (org-html-html5-fancy . t)
+	      (org-html-validation-link . nil))))
